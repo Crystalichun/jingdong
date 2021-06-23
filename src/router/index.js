f34db1424e9bdfe23d2a9f6aaa09b228f2055593
Crystal <1365991837@qq.com>
2021-06-23 18:24:28 +0800

    add login page layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-// import Home from '../views/Home.vue'
+import Home from '../views/home/Home'
+import Login from '../views/login/Login'
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
   // {
   //   path: '/about',
   //   name: 'About',
@@ -20,6 +26,16 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const { isLogin } = localStorage
+  const { name } = to
+  if (isLogin || name === 'Login') {
+    next()
+  } else {
+    next({ name: 'Login' })
+  }
 })
 
 export default router
---
+++ src/views/login/Login.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="wrapper">
+    <img class="wrapper__img" src="http://www.dell-lee.com/imgs/vue3/user.png"/>
+    <div class="wrapper__input">
+      <input class="wrapper__input__content"
+             placeholder="用户名"/>
+    </div>
+    <div class="wrapper__input">
+      <input
+            type="password"
+            class="wrapper__input__content"
+            placeholder="请输入密码"/>
+    </div>
+    <div class="wrapper__login-button" @click="handleLogin">登录</div>
+    <div class="wrapper__link">
+      <div class="wrapper__link-register">立即注册</div> | <div class="wrapper__link-password">忘记密码</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  setup () {
+    const handleLogin = () => {
+      console.log(1)
+    }
+    return { handleLogin }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../../style/viriables.scss";
+  .wrapper {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    right: 0;
+    transform: translateY(-50%);
+    &__img{
+      width: .66rem;
+      height: .66rem;
+      margin: 0 auto .4rem auto;
+      display: block;
+    }
+    &__input {
+      height: .48rem;
+      margin: 0 .4rem .16rem .4rem;
+      background: #F9F9F9;
+      border: 1px solid rgba(0,0,0,0.10);
+      border-radius: .06rem;
+      padding: 0 .16rem;
+      &__content{
+        width: 100%;
+        line-height: .48rem;
+        border: none;
+        outline: none;
+        background: none;
+        font-size: .16rem;
+        color: $content-notice-fontcolor;
+        &::placeholder {
+          color: $content-notice-fontcolor;
+        }
+      }
+    }
+    &__login-button {
+      margin: .32rem .4rem .16rem .4rem;
+      height: .48rem;
+      line-height: .48rem;
+      background: #0091FF;
+      box-shadow: 0 .04rem .08rem 0 rgba(0,145,255,0.32);
+      border-radius: .04rem;
+      color: #fff;
+      font-size: .16rem;
+      text-align: center;
+    }
+    &__link {
+      margin: .16rem .4rem;
+      text-align: center;
+      font-size: .14rem;
+      color: $content-notice-fontcolor;
+      &-register{
+        display: inline-block;
+        margin-right: .1rem;
+      }
+      &-password{
+        display: inline-block;
+        margin-left: .1rem;
+      }
+    }
+  }
+</style>
