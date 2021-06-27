f44ad6ed3ddd1306ee79bb0f6bbdb3099bea4ccf
Crystal <1365991837@qq.com>
2021-06-27 15:55:14 +0800

    add shop content

--- src/views/home/Nearby.vue
+++ src/views/home/Nearby.vue
@@ -1,49 +1,38 @@
 <template>
   <div class="nearby">
     <h3 class="nearby__title">附近店铺</h3>
-    <div class="nearby__item"
-         v-for="item in nearbyList"
-         :key="item.id">
-      <img class="nearby__item__img" :src="item.imgUrl">
-      <div class="nearby__content">
-        <div class="nearby__content__title">{{item.title}}</div>
-        <div class="nearby__content__tags">
-          <span class="nearby__content__tag"
-                v-for="(tag , index) in item.tags"
-                :key="index"
-          >
-          {{tag}}
-        </span>
-        </div>
-        <p class="nearby__content__highlight">{{item.desc}}</p>
-      </div>
-    </div>
+    <router-link
+      v-for="item in nearbyList"
+      :key="item._id"
+      :to="`/shop/${item._id}`"
+    >
+      <ShopInfo :item="item" />
+    </router-link>
   </div>
 </template>
 
 <script>
+import { get } from '@/utils/request'
+import { ref } from 'vue'
+import ShopInfo from '@/components/ShopInfo'
+
+const useNearbyEffect = () => {
+  const nearbyList = ref([])
+  const getNearbyList = async () => {
+    const result = await get('/api/shop/hot-list')
+    if (result?.errno === 0 && result?.data?.length) {
+      nearbyList.value = result.data
+    }
+  }
+  return { nearbyList, getNearbyList }
+}
+
 export default {
   name: 'Nearby',
+  components: { ShopInfo },
   setup () {
-    const nearbyList = [{
-      id: 1,
-      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }, {
-      id: 2,
-      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥5', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }, {
-      id: 3,
-      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥5', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }]
+    const { nearbyList, getNearbyList } = useNearbyEffect()
+    getNearbyList()
     return { nearbyList }
   }
 }
@@ -54,39 +43,13 @@ export default {
 
 .nearby {
   &__title {
-    font-size: 18px;
+    margin: .16rem 0 .02rem 0;
+    font-size: .18rem;
+    font-weight: normal;
+    color: $content-fontcolor;
   }
-  &__item {
-    display: flex;
-    &__img {
-      margin-right: .16rem;
-      width: .56rem;
-      height: .56rem;
-    }
-  }
-  &__content{
-    padding-bottom: .12rem;
-    border-bottom: 1px solid $content-bgColor;
-    &__title{
-      line-height: .22rem;
-      font-size: .16rem;
-      color: $content-fontcolor;
-    }
-    &__tags{
-      margin-top: .08rem;
-      line-height: .18rem;
-      font-size: .13rem;
-      color: $content-fontcolor;
-    }
-    &__tag{
-      margin-right: .16rem;
-    }
-    &__highlight {
-      margin: .08rem 0 0 0;
-      color: #E93B3B;
-      line-height: .18rem;
-      font-size: .13rem;
-    }
+  a {
+    text-decoration: none;
   }
 }
 </style>
---
+++ src/views/shop/Shop.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <div class="search__back iconfont" @click="handleBackClick">
+        &#xe6f2;
+      </div>
+      <div class="search__content">
+        <span class="search__content__icon iconfont ">&#xe62d;</span>
+        <input
+          class="search__content__input"
+          placeholder="请输入商品名称"
+        />
+      </div>
+    </div>
+    <ShopInfo :item="item" :hideBorder="true" v-show="item.imgUrl"/>
+    <ShopContent/>
+  </div>
+</template>
+
+<script>
+import ShopInfo from '../../components/ShopInfo'
+import { reactive, toRefs } from 'vue'
+import { get } from '@/utils/request'
+import { useRouter, useRoute } from 'vue-router'
+import ShopContent from '@/views/shop/ShopContent'
+
+const useShopInfoEffect = () => {
+  const route = useRoute()
+  const data = reactive({ item: {} })
+  const { item } = toRefs(data)
+  const getItemData = async () => {
+    const result = await get(`/api/shop/${route.params.id}`)
+    if (result?.errno === 0 && result?.data) {
+      data.item = result.data
+    }
+  }
+  return { item, getItemData }
+}
+
+const useBackRouterEffect = () => {
+  const router = useRouter()
+  const handleBackClick = () => {
+    router.back()
+  }
+  return handleBackClick
+}
+
+export default {
+  name: 'Shop',
+  components: { ShopContent, ShopInfo },
+  setup () {
+    const { item, getItemData } = useShopInfoEffect()
+    const handleBackClick = useBackRouterEffect()
+    getItemData()
+    return { item, handleBackClick }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../style/viriables.scss";
+.wrapper {
+  padding: 0 .18rem;
+}
+
+.search {
+  display: flex;
+  margin: .16rem 0 .04rem 0;
+  line-height: .32rem;
+  &__back {
+    width: .3rem;
+    font-size: .24rem;
+    color: #B6B6B6;
+  }
+  &__content {
+    display: flex;
+    flex:1;
+    background: $search-bgColor;
+    border-radius: .16rem;
+    &__icon {
+      width: .44rem;
+      text-align: center;
+      color: $search-fontColor;
+    }
+    &__input {
+      display: block;
+      height: .32rem;
+      width: 100%;
+      padding-right: .2rem;
+      outline: none;
+      border: none;
+      background: none;
+      font-size: .14rem;
+      color: $content-fontcolor;
+      &::placeholder {
+        color: $content-fontcolor;
+      }
+    }
+  }
+}
+</style>
